8f3ddd773e8aad0c7c9e08edaceb7265e8427b24
lea haim <leahaim36@gmail.com>
2025-11-05 16:07:11 +0200

    start favorite

---
+++ backend/src/services/favorite.services.ts
@@ -0,0 +1,39 @@
+import { CartModel } from "../models/cart.models";
+import { FavoriteModel } from "../models/favorite.models";
+import { IItemInCart } from "../types/cart.types";
+
+class FavoriteService {
+  async getAll(id: string): Promise<IFavorite[]> {
+    const favorite = await FavoriteModel.findOne({ user_id: id })
+
+    return favorite || [];
+  }
+  async addItemToCart(user_id: string, item_id: string, quantity: number) {
+    let cart = await CartModel.findOne({ user_id });
+    if (!cart) {
+      cart = new CartModel({ user_id, items: [{ item_id, quantity }] });
+    } else {
+      const item = cart.items.find((item) => item.item_id === item_id);
+      if (item) {
+        item.quantity += quantity;
+
+        if (item.quantity <= 0) {
+          this.deleteItem(user_id, item_id);
+        }
+      } else {
+        cart.items.push({ item_id, quantity });
+      }
+    }
+    await cart.save();
+    return await CartModel.findOne({ user_id });
+  }
+  async deleteItem(user_id: string, item_id: string) {
+    let cart = await CartModel.findOne({ user_id });
+    if (cart) {
+      cart.items = cart.items.filter((i) => i.item_id != item_id);
+    }
+    await cart?.save();
+    return await CartModel.findOne({ user_id });
+  }
+}
+export const FavoriteServiceInstance = new FavoriteService();
